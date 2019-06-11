0e1b78a0b10638fe36ef29ffd66163d7f244e9c6
KYIMINHAN <kyiminhan@gicjp.biz>
2019-06-11 13:50:19 +0900

    commit : change the database tables and attributes

--- spring-boot-mybatis-001/src/main/resources/db/kmh_test_db.sql
+++ spring-boot-mybatis-001/src/main/resources/db/kmh_test_db.sql
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `department`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `department` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `departmentname` varchar(45) DEFAULT NULL,
+  `departmentName` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
